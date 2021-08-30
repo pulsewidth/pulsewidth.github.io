afe497f327a8c409913991877cb09c4fc27d250d
pulsewidth <ernst@pulsewidth.ca>
2021-08-29 23:17:37 -0600

    maybe again

--- site.sh
+++ site.sh
@@ -49,17 +49,14 @@ for file in $list ; do
   cat end.htm_ >> ${target}
 
 
+
   # paginate
   if [ $((n % 19)) == 0 ]; then
     echo "--- page ---"
     echo "<br/><p><a href=/${log}n.html>[further]</a></p>" >> ${log}.html
-    echo "<br/><p><a href=/${radio}n.html>[further]</a></p>" >> ${radio}.html
     cat end.htm_ >> ${log}.html
-    cat end.htm_ >> ${radio}.html
     log=$log"n"
-    radio=$radio"n"
     cat start.htm_ > ${log}.html
-    cat start.htm_ > ${radio}.html
   fi
   ((n=n+1))
 
@@ -67,9 +64,7 @@ for file in $list ; do
   echo "<p><a href=${target}>${name}</a></p>" >> ${log}.html
   cmark ${file} >> ${log}.html
   echo "<br/>" >> ${log}.html
-  echo "<p><a href=${target}>${name}</a></p>" >> ${radio}.html
-  cmark ${file} >> ${radio}.html
-  echo "<br/>" >> ${radio}.html
+
 
   # append to rss
   echo "<item>" >> rss.xml
@@ -84,6 +79,12 @@ for file in $list ; do
   echo "</item>" >> rss.xml
 done
 
+
+cat end.htm_ >> ${log}.html
+cat end_rss.xml_ >> rss.xml
+
+# build radio, rss
+echo ">> build radio, rss"
 list2=$(ls -r ./radio/*.md)
 
 radio="radio"
@@ -110,21 +111,14 @@ for file in $list2 ; do
   # paginate
   if [ $((n % 19)) == 0 ]; then
     echo "--- page ---"
-    echo "<br/><p><a href=/${log}n.html>[further]</a></p>" >> ${log}.html
     echo "<br/><p><a href=/${radio}n.html>[further]</a></p>" >> ${radio}.html
-    cat end.htm_ >> ${log}.html
     cat end.htm_ >> ${radio}.html
-    log=$log"n"
     radio=$radio"n"
-    cat start.htm_ > ${log}.html
     cat start.htm_ > ${radio}.html
   fi
   ((n=n+1))
 
   # append to index
-  echo "<p><a href=${target}>${name}</a></p>" >> ${log}.html
-  cmark ${file} >> ${log}.html
-  echo "<br/>" >> ${log}.html
   echo "<p><a href=${target}>${name}</a></p>" >> ${radio}.html
   cmark ${file} >> ${radio}.html
   echo "<br/>" >> ${radio}.html
@@ -140,12 +134,11 @@ for file in $list2 ; do
   date=$(date -r $file "+%a, %d %b %Y 11:11:11 EST")
   echo "<pubDate>$date</pubDate>" >> rss.xml
   echo "</item>" >> rss.xml
-  done
+done
 
 
-cat end.htm_ >> ${log}.html
-cat end.htm_ >> ${radio}.html
-cat end_rss.xml_ >> rss.xml
+  cat end.htm_ >> ${radio}.html
+  cat end_rss.xml_ >> rss.xml
 
 # generate gmi
 
